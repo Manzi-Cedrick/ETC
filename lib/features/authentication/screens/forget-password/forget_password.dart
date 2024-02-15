e6cf1bacfff2c87fbb77a6c67b53da5177517093
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2024-02-15 23:17:51 +0200

    [core]: screens formation --update

---
+++ lib/features/authentication/screens/forget-password/forget_password.dart
@@ -0,0 +1,58 @@
+import 'package:etrade_actions/common/styles/spacing_styles.dart';
+import 'package:etrade_actions/features/authentication/screens/login/login.dart';
+import 'package:etrade_actions/features/authentication/screens/reset-password/reset_password.dart';
+import 'package:etrade_actions/utils/constants/image_strings.dart';
+import 'package:etrade_actions/utils/constants/sizes.dart';
+import 'package:etrade_actions/utils/constants/text_strings.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:iconsax/iconsax.dart';
+
+class ForgetPassword extends StatelessWidget {
+  const ForgetPassword({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(automaticallyImplyLeading: true),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: TSpacingStyle.paddingWithAppBarHeight,
+          child: Column(
+            children: [
+              Text(TTexts.forgetPasswordTitle,
+                  style: Theme.of(context).textTheme.headlineMedium,
+                  textAlign: TextAlign.center),
+              const SizedBox(height: TSizes.spaceBtwItems),
+              Text(TTexts.forgetPasswordSubTitle,
+                  style: Theme.of(context).textTheme.bodyMedium,
+                  textAlign: TextAlign.center),
+              const SizedBox(height: TSizes.spaceBtwSections * 2),
+              TextFormField(
+                decoration: const InputDecoration(
+                    prefixIcon: Icon(Iconsax.direct_right),
+                    labelText: TTexts.email),
+              ),
+              const SizedBox(height: TSizes.spaceBtwSections),
+              // Button
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () => Get.to(
+                    () => ResetPassword(
+                        image: TImages.deliveredEmailIllustration,
+                        title: TTexts.changeYourPasswordTitle,
+                        onPressed: () => Get.to(() => const LoginScreen()),
+                        subTitle: TTexts.changeYourPasswordSubTitle),
+                  ),
+                  child: const Text(TTexts.submit),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/features/authentication/screens/login/widgets/login_form.dart
@@ -0,0 +1,65 @@
+import 'package:etrade_actions/features/authentication/screens/forget-password/forget_password.dart';
+import 'package:etrade_actions/features/authentication/screens/signup/signup.dart';
+import 'package:etrade_actions/navigation_menu.dart';
+import 'package:etrade_actions/utils/constants/sizes.dart';
+import 'package:etrade_actions/utils/constants/text_strings.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:iconsax/iconsax.dart';
+
+class LoginForm extends StatelessWidget {
+  LoginForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: TSizes.spaceBtwSections),
+        child: Column(
+          children: [
+            TextFormField(
+              decoration: const InputDecoration(
+                  prefixIcon: Icon(Iconsax.direct_right),
+                  labelText: TTexts.email),
+            ),
+            const SizedBox(height: TSizes.spaceBtwInputFields / 2),
+            TextFormField(
+              decoration: const InputDecoration(
+                  prefixIcon: Icon(Iconsax.password_check),
+                  labelText: TTexts.password,
+                  suffixIcon: Icon(Iconsax.eye_slash)),
+            ),
+            const SizedBox(height: TSizes.spaceBtwInputFields / 2),
+            // Remember Me & Forget Passowrd
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Checkbox(value: true, onChanged: (value) {}),
+                    const Text(TTexts.rememberMe),
+                  ],
+                ),
+                TextButton(
+                    onPressed: () => Get.to(() => const ForgetPassword()), child: const Text(TTexts.forgetPassword)),
+              ],
+            ),
+            // Sign In
+            const SizedBox(height: TSizes.spaceBtwSections),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                  onPressed: () => Get.to(() => const NavigationMenu()), child: const Text(TTexts.signIn)),
+            ),
+            const SizedBox(height: TSizes.spaceBtwItems),
+            SizedBox(
+              width: double.infinity,
+              child: OutlinedButton(
+                  onPressed: () => Get.to(() => const SignupScreen()), child: const Text(TTexts.createAccount)),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
