241745e6596984055cb4edb1b92e2b9f04fc3546
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2024-02-15 23:29:54 +0200

    [fix]: constant showcase --update

--- lib/utils/constants/api_constants.dart
+++ lib/utils/constants/api_constants.dart
@@ -2,4 +2,4 @@
 /* -- LIST OF Constants used in APIs -- */
 
 // Example
-const String tSecretAPIKey = "cwt_live_b2da6ds3df3e785v8ddc59198f7615ba";
+const String tSecretAPIKey = String.fromEnvironment('T_SECRET_API_KEY');
