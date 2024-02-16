8450aeec730e2d66d103598bd00ad2ed6230b64f
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2024-02-16 23:38:18 +0200

    [feat]: structure -update

--- lib/features/shop/screens/home/home_screen.dart
+++ lib/features/shop/screens/home/home_screen.dart
@@ -1,7 +1,11 @@
+import 'package:etrade_actions/common/product/product_cards/product_card_vertical.dart';
+import 'package:etrade_actions/features/shop/screens/home/widgets/TGriedViewLayout.dart';
 import 'package:etrade_actions/features/shop/screens/home/widgets/THomeAppBar.dart';
 import 'package:etrade_actions/features/shop/screens/home/widgets/THomeCategory.dart';
 import 'package:etrade_actions/features/shop/screens/home/widgets/TPrimaryHeader.dart';
+import 'package:etrade_actions/features/shop/screens/home/widgets/TPromoSlider.dart';
 import 'package:etrade_actions/features/shop/screens/home/widgets/TSearchStore.dart';
+import 'package:etrade_actions/features/shop/screens/home/widgets/TSectionHeading.dart';
 import 'package:etrade_actions/utils/constants/image_strings.dart';
 import 'package:etrade_actions/utils/constants/sizes.dart';
 import 'package:flutter/material.dart';
@@ -20,22 +24,36 @@ class HomeScreen extends StatelessWidget {
                 children: [
                   const THomeAppBar(),
                   const SizedBox(height: TSizes.spaceBtwSections),
-                  TStoreSearch(displayText: 'Search in store', onTap: () {},),
+                  TStoreSearch(
+                    displayText: 'Search in store',
+                    onTap: () {},
+                  ),
                   // Categories
                   const THomeCategory(),
                 ],
               ),
             ),
             Padding(
-              padding: const EdgeInsets.symmetric(horizontal: TSizes.defaultSpace),
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(TSizes.md),
-                ),
-                child: const Image(
-                  image: AssetImage(TImages.promoBanner2),
-                  fit: BoxFit.cover,
-                ),
+              padding:
+                  const EdgeInsets.symmetric(horizontal: TSizes.spaceBtwItems),
+              child: Column(
+                children: [
+                  const TPromoSlider(
+                    banners: [
+                      TImages.promoBanner1,
+                      TImages.promoBanner2,
+                      TImages.promoBanner3,
+                    ],
+                  ),
+                  const SizedBox(height: TSizes.spaceBtwItems),
+                  TSectionHeading(
+                    title: 'Popular Products',
+                    showActionsButton: true,
+                    onButtonPressed: () {},
+                  ),
+                  const SizedBox(height: TSizes.spaceBtwItems),
+                  const TGridViewLayout(),
+                ],
               ),
             )
           ],
