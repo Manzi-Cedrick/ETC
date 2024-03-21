3e0fc153876ebf90b6bc623ce971f052d9cda931
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2024-03-21 18:40:30 +0200

    [core]: product cart manipulate --addition

---
+++ lib/features/shop/screens/cart/widgets/product_quantity.dart
@@ -0,0 +1,42 @@
+import 'package:etrade_actions/common/widgets/icon/circular_icon.dart';
+import 'package:etrade_actions/utils/constants/colors.dart';
+import 'package:etrade_actions/utils/constants/sizes.dart';
+import 'package:etrade_actions/utils/helpers/helper_functions.dart';
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+
+class TProductQuantityWithAddRemoveButton extends StatelessWidget {
+  const TProductQuantityWithAddRemoveButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        TCicularIcon(
+          icon: Iconsax.minus,
+          width: 32,
+          height: 32,
+          size: TSizes.md,
+          color: THelperFunctions.isDarkMode(context)
+              ? TColors.white
+              : TColors.black,
+          backgroundColor: THelperFunctions.isDarkMode(context)
+              ? TColors.darkGrey
+              : TColors.light,
+        ),
+        const SizedBox(width: TSizes.spaceBtwItems),
+        Text('2', style: Theme.of(context).textTheme.titleSmall),
+        const SizedBox(width: TSizes.spaceBtwItems),
+        const TCicularIcon(
+          icon: Iconsax.add,
+          width: 32,
+          height: 32,
+          size: TSizes.md,
+          color: TColors.white,
+          backgroundColor: TColors.primary,
+        ),
+      ],
+    );
+  }
+}
