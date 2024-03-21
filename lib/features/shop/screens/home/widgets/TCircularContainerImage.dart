6dd64c418a0dbfd986a277b0b8aa68604bdecc22
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2024-03-21 18:42:04 +0200

    [core]: clip svg --addition

--- lib/features/shop/screens/home/widgets/TCircularContainerImage.dart
+++ lib/features/shop/screens/home/widgets/TCircularContainerImage.dart
@@ -1,29 +1,35 @@
 import 'package:etrade_actions/utils/constants/sizes.dart';
 import 'package:flutter/material.dart';
 
-class TCircularContainerImage extends StatelessWidget {
-  const TCircularContainerImage({
-    Key? key,
-    this.width = 56,
-    this.height = 56,
-    this.overlayColor,
-    this.backgroundColor,
-    required this.image,
-    this.isNetworkImage = false,
-    this.onTap,
-    this.radius = 100,
-    this.fit = BoxFit.cover,
-    this.padding,
-  }) : super(key: key);
+class TRoundedImage extends StatelessWidget {
+  const TRoundedImage(
+      {Key? key,
+      this.width = 56,
+      this.height = 56,
+      this.overlayColor,
+      this.backgroundColor,
+      required this.image,
+      this.padding = TSizes.sm,
+      this.isNetworkImage = false,
+      this.onTap,
+      this.radius = TSizes.sm,
+      this.fit = BoxFit.cover,
+      this.showMargin = false,
+      this.applyImageRadius = true,
+      this.border})
+      : super(key: key);
 
-  final double? width, height, padding;
+  final double? width, height;
+  final double padding;
   final double radius;
   final BoxFit? fit;
   final String image;
-  final bool isNetworkImage;
+  final bool isNetworkImage, showMargin;
   final VoidCallback? onTap;
   final Color? overlayColor;
   final Color? backgroundColor;
+  final Border? border;
+  final bool applyImageRadius;
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -31,16 +37,25 @@ class TCircularContainerImage extends StatelessWidget {
       child: Container(
         height: height,
         width: width,
-        padding: EdgeInsets.all(padding ?? TSizes.sm),
-        margin: const EdgeInsets.symmetric(horizontal: TSizes.defaultSpace),
+        padding: EdgeInsets.all(padding),
+        margin: showMargin
+            ? const EdgeInsets.symmetric(horizontal: TSizes.defaultSpace)
+            : null,
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(radius),
+          border: border,
+          color: backgroundColor
         ),
-        child: Image(
-          fit: fit,
-          image: isNetworkImage ? NetworkImage(image) : AssetImage(image) as ImageProvider,
-          color: overlayColor,
-          // fit: BoxFit.cover,
+        child: ClipRRect(
+          borderRadius: applyImageRadius ? BorderRadius.circular(radius) : BorderRadius.zero,
+          child: Image(
+            fit: fit,
+            image: isNetworkImage
+                ? NetworkImage(image)
+                : AssetImage(image) as ImageProvider,
+            color: overlayColor,
+            // fit: BoxFit.cover,
+          ),
         ),
       ),
     );
