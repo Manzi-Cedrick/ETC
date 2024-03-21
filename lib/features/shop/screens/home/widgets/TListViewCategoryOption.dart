8a8ff01872b8b01894bffc99d7f93c8862903982
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2024-03-21 18:42:19 +0200

    [core]: updates --addition

--- lib/features/shop/screens/home/widgets/TListViewCategoryOption.dart
+++ lib/features/shop/screens/home/widgets/TListViewCategoryOption.dart
@@ -1,6 +1,8 @@
 import 'package:etrade_actions/features/shop/screens/home/widgets/TVerticalCard.dart';
+import 'package:etrade_actions/features/shop/screens/sub_category/sub_category.dart';
 import 'package:etrade_actions/utils/constants/image_strings.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class TListViewCategoryOption extends StatelessWidget {
   const TListViewCategoryOption({super.key});
@@ -13,7 +15,7 @@ class TListViewCategoryOption extends StatelessWidget {
         itemCount: 6,
         shrinkWrap: true,
         scrollDirection: Axis.horizontal,
-        itemBuilder: (_, index) => const TVerticalCard(image: TImages.sportIcon, title: 'Shoes')
+        itemBuilder: (_, index) => TVerticalCard(image: TImages.sportIcon, title: 'Shoes', onTap: () => Get.to(() => const SubCategoriesScreen()),)
       ),
     );
   }
