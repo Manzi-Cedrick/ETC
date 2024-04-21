6d3b0de166c1cce6a78cd08c158b5daf1a903859
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2024-04-21 22:53:37 +0200

    [core]: updates

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,4 @@
-import 'package:etrade_actions/common/widgets/bindings/general_bindings.dart';
+import 'package:etrade_actions/bindings/general_bindings.dart';
 import 'package:etrade_actions/utils/constants/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
