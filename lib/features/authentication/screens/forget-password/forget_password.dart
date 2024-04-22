6e7b45c3157dd3952e0c31fd242e75ffd319a87d
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2024-04-22 23:02:50 +0200

    [core]: functionality cross update

--- lib/features/authentication/screens/forget-password/forget_password.dart
+++ lib/features/authentication/screens/forget-password/forget_password.dart
@@ -1,9 +1,8 @@
 import 'package:etrade_actions/common/styles/spacing_styles.dart';
-import 'package:etrade_actions/features/authentication/screens/login/login.dart';
-import 'package:etrade_actions/features/authentication/screens/reset-password/reset_password.dart';
-import 'package:etrade_actions/utils/constants/image_strings.dart';
+import 'package:etrade_actions/features/authentication/controllers/forget_password/forget_password_controller.dart';
 import 'package:etrade_actions/utils/constants/sizes.dart';
 import 'package:etrade_actions/utils/constants/text_strings.dart';
+import 'package:etrade_actions/utils/validators/validation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
@@ -13,6 +12,7 @@ class ForgetPassword extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(ForgetPasswordController());
     return Scaffold(
       appBar: AppBar(automaticallyImplyLeading: true),
       body: SingleChildScrollView(
@@ -28,23 +28,22 @@ class ForgetPassword extends StatelessWidget {
                   style: Theme.of(context).textTheme.bodyMedium,
                   textAlign: TextAlign.center),
               const SizedBox(height: TSizes.spaceBtwSections * 2),
-              TextFormField(
-                decoration: const InputDecoration(
-                    prefixIcon: Icon(Iconsax.direct_right),
-                    labelText: TTexts.email),
+              Form(
+                key: controller.forgetPasswordFormKey,
+                child: TextFormField(
+                  validator: (value) => TValidator.validateEmail(value),
+                  controller: controller.email,
+                  decoration: const InputDecoration(
+                      prefixIcon: Icon(Iconsax.direct_right),
+                      labelText: TTexts.email),
+                ),
               ),
               const SizedBox(height: TSizes.spaceBtwSections),
               // Button
               SizedBox(
                 width: double.infinity,
                 child: ElevatedButton(
-                  onPressed: () => Get.to(
-                    () => ResetPassword(
-                        image: TImages.deliveredEmailIllustration,
-                        title: TTexts.changeYourPasswordTitle,
-                        onPressed: () => Get.to(() => const LoginScreen()),
-                        subTitle: TTexts.changeYourPasswordSubTitle),
-                  ),
+                  onPressed: () => controller.sendPasswordResetEmail(),
                   child: const Text(TTexts.submit),
                 ),
               ),
