78c50e6d50b8eb02807309deda52c6526d0cfb61
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2024-04-22 23:03:51 +0200

    [core]: functional based updates

--- lib/features/authentication/screens/signup/widgets/terms_policy.dart
+++ lib/features/authentication/screens/signup/widgets/terms_policy.dart
@@ -1,17 +1,22 @@
+import 'package:etrade_actions/features/authentication/controllers/signup/signup_controller.dart';
 import 'package:etrade_actions/utils/constants/colors.dart';
+import 'package:etrade_actions/utils/constants/sizes.dart';
 import 'package:etrade_actions/utils/constants/text_strings.dart';
 import 'package:etrade_actions/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class TTermsPolicy extends StatelessWidget {
   const TTermsPolicy({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final controller = SignupController.instance;
     final dark = THelperFunctions.isDarkMode(context);
     return Row(
       children: [
-        Checkbox(value: true, onChanged: (value) {}),
+        SizedBox(width: 24,height: 24,child: Obx(() => Checkbox(value: controller.privacyPolicy.value, onChanged: (value) => controller.privacyPolicy.value = !controller.privacyPolicy.value))),
+        const SizedBox(width: TSizes.spaceBtwItems),
         Text.rich(TextSpan(children: [
           TextSpan(
               text: '${TTexts.iAgreeTo} ',
