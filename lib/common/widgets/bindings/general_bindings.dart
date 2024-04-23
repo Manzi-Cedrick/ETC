cfc915dfcb662f323feb0b4e1615b35cda4f5700
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2024-04-24 00:22:39 +0200

    [core]: multi phased changes --update

--- lib/common/widgets/bindings/general_bindings.dart
+++
@@ -1,9 +0,0 @@
-import 'package:etrade_actions/utils/helpers/network_manager.dart';
-import 'package:get/get.dart';
-
-class GeneralBindings extends Bindings {
-  @override
-  void dependencies() {
-    Get.put(NetworkManager());
-  }
-}
