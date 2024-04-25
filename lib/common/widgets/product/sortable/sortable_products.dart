b2d913db021a9328f5c2eab5a6e586174eda22ab
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2024-04-25 23:26:46 +0200

    [core]: functioning program --update

--- lib/common/widgets/product/sortable/sortable_products.dart
+++ lib/common/widgets/product/sortable/sortable_products.dart
@@ -1,13 +1,19 @@
 import 'package:etrade_actions/common/widgets/product/product_cards/product_card_vertical.dart';
+import 'package:etrade_actions/features/shop/controllers/all_products_controller.dart';
+import 'package:etrade_actions/features/shop/models/product_model.dart';
 import 'package:etrade_actions/features/shop/screens/home/widgets/TGriedViewLayout.dart';
 import 'package:etrade_actions/utils/constants/sizes.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class TSortableProducts extends StatelessWidget {
-  const TSortableProducts({super.key});
+  const TSortableProducts({super.key, required this.products});
 
+  final List<ProductModel> products;
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(AllProductController());
+    controller.assignProducts(products);
     return Column(
       children: [
         DropdownButtonFormField(
@@ -24,15 +30,21 @@ class TSortableProducts extends StatelessWidget {
                     child: Text(e),
                   ))
               .toList(),
-          onChanged: (value) {},
+          value: controller.selectedSortOption.value,
+          onChanged: (value) {
+            controller.sortProducts(value!);
+          },
           hint: const Text('Select category'),
         ),
         const SizedBox(height: TSizes.spaceBtwSections),
-        // TGridViewLayout(
-        //     itemCount: 10,
-        //     itemBuilder: (_, index) {
-        //       return const TProductCartVertical();
-        //     })
+        Obx(
+          () => TGridViewLayout(
+              itemCount: controller.products.length,
+              itemBuilder: (_, index) {
+                return TProductCartVertical(
+                    product: controller.products[index]);
+              }),
+        )
       ],
     );
   }
