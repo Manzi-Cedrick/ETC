eac9cf772011b6f0017f694821534110b6e4161c
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2024-04-25 23:26:35 +0200

    [core]: functioning program --update

--- lib/common/widgets/brands/TBrands.dart
+++ lib/common/widgets/brands/TBrands.dart
@@ -1,19 +1,21 @@
 import 'package:etrade_actions/common/widgets/custom_shapes/circular_container.dart';
 import 'package:etrade_actions/common/widgets/product/cart/t_brand_title_text_with_vertical_icon.dart';
+import 'package:etrade_actions/features/shop/models/brand_model.dart';
 import 'package:etrade_actions/features/shop/screens/home/widgets/TCircularContainerImage.dart';
 import 'package:etrade_actions/utils/constants/colors.dart';
 import 'package:etrade_actions/utils/constants/enums.dart';
-import 'package:etrade_actions/utils/constants/image_strings.dart';
 import 'package:etrade_actions/utils/constants/sizes.dart';
 import 'package:etrade_actions/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
 
 class TBrandCard extends StatelessWidget {
-  const TBrandCard({Key? key, this.onTap, required this.showBorder})
+  const TBrandCard(
+      {Key? key, this.onTap, required this.showBorder, required this.brand})
       : super(key: key);
 
   final void Function()? onTap;
   final bool showBorder;
+  final BrandModel brand;
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -29,8 +31,7 @@ class TBrandCard extends StatelessWidget {
           children: [
             TRoundedImage(
               isNetworkImage: false,
-              image: TImages.clothIcon,
-              onTap: () {},
+              image: brand.image,
               fit: BoxFit.contain,
               width: 38,
               overlayColor: THelperFunctions.isDarkMode(context)
@@ -42,12 +43,12 @@ class TBrandCard extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                const TBrandTitleWithVerificationIcon(
-                  title: 'Nike',
+                TBrandTitleWithVerificationIcon(
+                  title: brand.name,
                   brandTextSize: TextSizes.large,
                   textAlign: TextAlign.start,
                 ),
-                Text('256 products',
+                Text('${brand.productsCount} products',
                     style: Theme.of(context).textTheme.labelMedium),
               ],
             ),
