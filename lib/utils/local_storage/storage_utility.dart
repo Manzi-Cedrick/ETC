58732d9df91578916526b264966378028ed4998d
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2024-04-26 23:11:10 +0200

    [core]: updates functionality --addition

--- lib/utils/local_storage/storage_utility.dart
+++ lib/utils/local_storage/storage_utility.dart
@@ -1,15 +1,21 @@
 import 'package:get_storage/get_storage.dart';
 
 class TLocalStorage {
-  static final TLocalStorage _instance = TLocalStorage._internal();
+  late final GetStorage _storage;
+  static TLocalStorage? _instance;
 
-  factory TLocalStorage() {
-    return _instance;
+  factory TLocalStorage.instance() {
+    _instance ??= TLocalStorage._internal();
+    return _instance!;
   }
 
   TLocalStorage._internal();
 
-  final _storage = GetStorage();
+  static Future<void> init(String bucketName) async {
+    await GetStorage.init(bucketName);
+    _instance = TLocalStorage._internal();
+    _instance!._storage = GetStorage(bucketName);
+  }
 
   // Generic method to save data
   Future<void> saveData<T>(String key, T value) async {
