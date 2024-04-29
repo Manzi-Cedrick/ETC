d18838e6d55a73d5f3d43be7a09f7af1c986ab78
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2024-04-29 22:34:32 +0200

    [core]: integration changes activation --addition

--- lib/common/widgets/product/cart/cart_item.dart
+++ lib/common/widgets/product/cart/cart_item.dart
@@ -1,21 +1,23 @@
 import 'package:etrade_actions/common/widgets/product/cart/t_brand_title_text_with_vertical_icon.dart';
 import 'package:etrade_actions/common/widgets/texts/product_title_text.dart';
+import 'package:etrade_actions/features/shop/models/cart_item_model.dart';
 import 'package:etrade_actions/features/shop/screens/home/widgets/TCircularContainerImage.dart';
 import 'package:etrade_actions/utils/constants/colors.dart';
-import 'package:etrade_actions/utils/constants/image_strings.dart';
 import 'package:etrade_actions/utils/constants/sizes.dart';
 import 'package:etrade_actions/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
 
 class TCartItem extends StatelessWidget {
-  const TCartItem({super.key});
+  const TCartItem({super.key, required this.cartItemModel});
 
+  final CartItemModel cartItemModel;
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
         TRoundedImage(
-          image: TImages.productImage1,
+          image: cartItemModel.image ?? '',
+          isNetworkImage: true,
           width: 60,
           height: 60,
           padding: TSizes.md,
@@ -29,27 +31,30 @@ class TCartItem extends StatelessWidget {
             mainAxisSize: MainAxisSize.min,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const TBrandTitleWithVerificationIcon(title: 'Nike'),
-              const Flexible(
+              TBrandTitleWithVerificationIcon(
+                  title: cartItemModel.brandName ?? ''),
+              Flexible(
                   child: TProductTitleText(
-                      title: 'Black Sports shoes online with excluded quality',
+                      title: cartItemModel.title ?? '',
                       overflow: TextOverflow.ellipsis,
                       maxLines: 1)),
 
               // Attributes
-              Text.rich(TextSpan(children: [
+              Text.rich(
                 TextSpan(
-                    text: 'Color',
-                    style: Theme.of(context).textTheme.bodySmall),
-                TextSpan(
-                    text: ' Green ',
-                    style: Theme.of(context).textTheme.bodyLarge),
-                TextSpan(
-                    text: 'Size', style: Theme.of(context).textTheme.bodySmall),
-                TextSpan(
-                    text: ' UK 08',
-                    style: Theme.of(context).textTheme.bodyLarge),
-              ]))
+                  children: (cartItemModel.selectedVariation ?? {})
+                      .entries
+                      .map((e) => TextSpan(children: [
+                            TextSpan(
+                                text: e.key,
+                                style: Theme.of(context).textTheme.bodySmall),
+                            TextSpan(
+                                text: e.value,
+                                style: Theme.of(context).textTheme.bodyLarge)
+                          ]))
+                      .toList(),
+                ),
+              ),
             ],
           ),
         )
