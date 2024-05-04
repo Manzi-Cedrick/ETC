23b6ca02b710ed6f52eb5873a6e428868beb6274
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2024-05-04 22:58:56 +0200

    [core]: updates --addition

--- lib/features/shop/screens/checkout/checkout_screen.dart
+++ lib/features/shop/screens/checkout/checkout_screen.dart
@@ -1,16 +1,17 @@
 import 'package:etrade_actions/common/widgets/appbar/appbar.dart';
 import 'package:etrade_actions/common/widgets/coupon/coupon_code.dart';
 import 'package:etrade_actions/common/widgets/custom_shapes/circular_container.dart';
-import 'package:etrade_actions/features/authentication/screens/success/success_screen%20copy.dart';
+import 'package:etrade_actions/common/widgets/loaders/loaders.dart';
+import 'package:etrade_actions/features/shop/controllers/cart_controller.dart';
+import 'package:etrade_actions/features/shop/controllers/order_controller.dart';
 import 'package:etrade_actions/features/shop/screens/cart/widgets/multiple_cart_items.dart';
 import 'package:etrade_actions/features/shop/screens/checkout/widgets/billing_address_section.dart';
 import 'package:etrade_actions/features/shop/screens/checkout/widgets/billing_amount_section.dart';
 import 'package:etrade_actions/features/shop/screens/checkout/widgets/billing_payment_section.dart';
-import 'package:etrade_actions/navigation_menu.dart';
 import 'package:etrade_actions/utils/constants/colors.dart';
-import 'package:etrade_actions/utils/constants/image_strings.dart';
 import 'package:etrade_actions/utils/constants/sizes.dart';
 import 'package:etrade_actions/utils/helpers/helper_functions.dart';
+import 'package:etrade_actions/utils/helpers/pricing_calculator.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -20,6 +21,10 @@ class CheckoutScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final dark = THelperFunctions.isDarkMode(context);
+    final cartController = CartController.instance;
+    final subTotal = cartController.totalCartPrice.value;
+    final orderController = Get.put(OrderController());
+    final totalAmount = TPricingCalculator.calculateTotalPrice(subTotal, 'US');
     return Scaffold(
       appBar: TAppBar(
           showBackArrow: true,
@@ -41,19 +46,22 @@ class CheckoutScreen extends StatelessWidget {
                 padding: const EdgeInsets.all(TSizes.md),
                 backgroundColor: dark ? TColors.black : TColors.white,
                 child: const Column(
-                  // Pricing
                   children: [
+                    // Pricing
                     TBillingAmountSection(),
                     SizedBox(height: TSizes.spaceBtwItems),
+
+                    // Divider
                     Divider(),
                     SizedBox(height: TSizes.spaceBtwItems),
+
+                    // Payment methods
                     TBillingPaymentSection(),
                     SizedBox(height: TSizes.spaceBtwItems),
+
+                    // Billing Address
                     TBillingAddressSection()
                   ],
-                  // Dividerp
-                  // Payment
-                  // Address
                 ),
               )
             ],
@@ -63,13 +71,13 @@ class CheckoutScreen extends StatelessWidget {
       bottomNavigationBar: Padding(
         padding: const EdgeInsets.all(TSizes.defaultSpace),
         child: ElevatedButton(
-          onPressed: () => Get.to(() => SucessScreen(
-                image: TImages.successfulPaymentIcon,
-                title: 'Payment Success!',
-                subTitle: 'Your item will be shipped soon!',
-                onPressed: () => Get.offAll(() => const NavigationMenu()),
-              )),
-          child: const Text('Checkout \$256.0'),
+          onPressed: subTotal > 0
+              ? () => orderController.processOrder(totalAmount)
+              : () => TLoaders.warningSnackBar(
+                  title: 'Empty Cart',
+                  message: 'Add items in the cart in order to proceed.'),
+          child: Text(
+              'Checkout \$${TPricingCalculator.calculateTotalPrice(subTotal, 'US')}'),
         ),
       ),
     );
